96f4cb300ae1c370978823c8f735b10553bcf3d3
Oleh Hrubnik <55631021+Suprelf@users.noreply.github.com>
2024-05-29 05:56:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
-from tkinter import Tk
+import time
+import tkinter as tk
+from math import sqrt, floor
 from tkinter.filedialog import askopenfilename
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt, floor
-import time
 from PIL import Image
+
 
 def read_image(path):
     img = plt.imread(path)  # cv2.IMREAD_GRAYSCALE)
     size = img.shape
     dimension = (size[0], size[1])
 
-    return img[...,::-1], size, dimension
+    return img[..., ::-1], size, dimension
 
 
 def image_change_scale(img, dimension, scale=100, interpolation=cv2.INTER_LINEAR):
@@ -37,12 +38,12 @@ def nearest_interpolation(image, dimension):
             row = floor(i / enlarge_time)
             column = floor(j / enlarge_time)
 
-            new_image[i, j] = image[row-1, column-1]
+            new_image[i, j] = image[row - 1, column - 1]
 
     end_time = time.time()
     print(f"Nearest time: {round(end_time - start_time, 4)}")
 
-    return new_image[...,::-1]
+    return new_image[..., ::-1]
 
 
 def bilinear_interpolation(image, dimension):
@@ -85,7 +86,7 @@ def bilinear_interpolation(image, dimension):
     end_time = time.time()
     print(f"Bilinear time: {round(end_time - start_time, 4)}")
 
-    return new_image[...,::-1]
+    return new_image[..., ::-1]
 
 
 def W(x):
@@ -131,62 +132,151 @@ def bicubic_interpolation(img, dimension):
     end_time = time.time()
     print(f"Bicubic time: {round(end_time - start_time, 5)}")
 
-    return output[...,::-1]
+    return output[..., ::-1]
 
 
 def main():
+    mode = input("Select mode: \nUpscale\nResearch\nCompare\n-> ").lower()
 
-    # Select File
-    Tk().withdraw()
-    filename = askopenfilename()
-    print(filename)
+    if (mode == "upscale"):
+        print("Upscale mode")
 
-    # Read Image
-    img, size, dimension = read_image(filename) # "./test.jpg"
-    print(f"Image size is: {size}")
+        # Select File
+        root = tk.Tk()
+        root.withdraw()
+        root.call('wm', 'attributes', '.', '-topmost', True)
+        filename = askopenfilename()
+        print(filename)
 
-    # Change Image Size
-    scale_percent = 25  # percent of original image size
-    resized_img = image_change_scale(img, dimension, scale_percent)
-    print(f"Smalled Image size is: {resized_img.shape}")
-    print(f"Worked pixels: {resized_img.shape[0]*resized_img.shape[1]}")
+        # Read Image
+        img, size, dimension = read_image(filename)  # "./test.jpg"
+        print(f"Image size is: {size}")
 
-    fig, axs = plt.subplots(2, 3)
+        # Change Image Size
+        scale_percent = int(input("Enter upscale size in %: "))  # percent of original image size
+        resized_img = image_change_scale(img, dimension, scale_percent)
+        print(f"Bigger Image size is: {resized_img.shape}")
+        print(f"Worked pixels: {resized_img.shape[0] * resized_img.shape[1]}")
+        dimension = np.array(dimension)
+        dimension = dimension * (int(scale_percent / 100))
 
-    nn_img_algo = nearest_interpolation(resized_img, dimension)
-    nn_img_algo = Image.fromarray(nn_img_algo.astype('uint8'))
-    nn_img_algo.save("n_" + os.path.basename(os.path.normpath(filename)))
+        fig, axs = plt.subplots(1, 1)
 
-    bil_img_algo = bilinear_interpolation(resized_img, dimension)
-    bil_img_algo = Image.fromarray(bil_img_algo.astype('uint8'))
-    bil_img_algo.save("l_" + os.path.basename(os.path.normpath(filename)))
+        nn_img_algo = nearest_interpolation(img, dimension)
+        nn_img_algo = Image.fromarray(nn_img_algo.astype('uint8'))
+        nn_img_algo.save(str(scale_percent) + "_n_" + os.path.basename(os.path.normpath(filename)))
 
-    cubic_img_algo = bicubic_interpolation(resized_img, dimension)
-    cubic_img_algo = Image.fromarray(cubic_img_algo.astype('uint8'))
-    cubic_img_algo.save("b_" + os.path.basename(os.path.normpath(filename)))
+        bil_img_algo = bilinear_interpolation(img, dimension)
+        bil_img_algo = Image.fromarray(bil_img_algo.astype('uint8'))
+        bil_img_algo.save(str(scale_percent) + "_l_" + os.path.basename(os.path.normpath(filename)))
 
-    axs[0, 0].set_title("Original")
-    axs[0, 0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        cubic_img_algo = bicubic_interpolation(img, dimension)
+        cubic_img_algo = Image.fromarray(cubic_img_algo.astype('uint8'))
+        cubic_img_algo.save(str(scale_percent) + "_b_" + os.path.basename(os.path.normpath(filename)))
 
-    axs[1, 0].set_title(f"Smaller: {scale_percent}%")
-    axs[1, 0].imshow(cv2.cvtColor(np.array(resized_img), cv2.COLOR_BGR2RGB))
+        axs[0, 0].set_title("Original")
+        axs[0, 0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
-    axs[0, 1].set_title("Nearest")
-    axs[0, 1].imshow(nn_img_algo)
+        axs[0, 1].set_title("Nearest")
+        axs[0, 1].imshow(nn_img_algo)
 
-    axs[0, 2].set_title("Bilinear")
-    axs[0, 2].imshow(bil_img_algo)
+        axs[0, 0].set_title("Bilinear")
+        axs[1, 1].imshow(bil_img_algo)
 
-    axs[1, 1].set_title("Bicubic")
-    axs[1, 1].imshow(cubic_img_algo)
+        axs[1, 0].set_title("Bicubic")
+        axs[1, 0].imshow(cubic_img_algo)
 
-    axs[1, 2].set_title("Lanczos (TODO)")
-    axs[1, 2].imshow(cv2.cvtColor(np.array(resized_img), cv2.COLOR_BGR2RGB))
+        plt.show()
 
-    plt.show()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if (mode == "research"):
+        print("Research mode")
+
+        # Select File
+        root = tk.Tk()
+        root.withdraw()
+        root.call('wm', 'attributes', '.', '-topmost', True)
+        filename = askopenfilename()
+        print(filename)
+
+        # Read Image
+        img, size, dimension = read_image(filename)  # "./test.jpg"
+        print(f"Image size is: {size}")
+
+        # Change Image Size
+        scale_percent = 25  # percent of original image size
+        resized_img = image_change_scale(img, dimension, scale_percent)
+        print(f"Smalled Image size is: {resized_img.shape}")
+        print(f"Worked pixels: {resized_img.shape[0] * resized_img.shape[1]}")
+
+        fig, axs = plt.subplots(2, 3)
+
+        nn_img_algo = nearest_interpolation(resized_img, dimension)
+        nn_img_algo = Image.fromarray(nn_img_algo.astype('uint8'))
+        nn_img_algo.save("n_" + os.path.basename(os.path.normpath(filename)))
+
+        bil_img_algo = bilinear_interpolation(resized_img, dimension)
+        bil_img_algo = Image.fromarray(bil_img_algo.astype('uint8'))
+        bil_img_algo.save("l_" + os.path.basename(os.path.normpath(filename)))
+
+        cubic_img_algo = bicubic_interpolation(resized_img, dimension)
+        cubic_img_algo = Image.fromarray(cubic_img_algo.astype('uint8'))
+        cubic_img_algo.save("b_" + os.path.basename(os.path.normpath(filename)))
+
+        axs[0, 0].set_title("Original")
+        axs[0, 0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+        axs[1, 0].set_title(f"Smaller: {scale_percent}%")
+        axs[1, 0].imshow(cv2.cvtColor(np.array(resized_img), cv2.COLOR_BGR2RGB))
+
+        axs[0, 1].set_title("Nearest")
+        axs[0, 1].imshow(nn_img_algo)
+
+        axs[0, 2].set_title("Bilinear")
+        axs[0, 2].imshow(bil_img_algo)
+
+        axs[1, 1].set_title("Bicubic")
+        axs[1, 1].imshow(cubic_img_algo)
+
+        axs[1, 2].set_title("Lanczos (TODO)")
+        axs[1, 2].imshow(cv2.cvtColor(np.array(resized_img), cv2.COLOR_BGR2RGB))
+
+        plt.show()
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    if (mode == "compare"):
+        print("Compare mode")
+
+        root = tk.Tk()
+        root.withdraw()
+        root.call('wm', 'attributes', '.', '-topmost', True)
+        filename1 = askopenfilename()
+        print(filename1)
+
+        img1, size1, dimension1 = read_image(filename1)
+        img1 = Image.fromarray(img1.astype('uint8'))
+        print(f"Image selected: {os.path.basename(os.path.normpath(filename1))}")
+
+        print("Select second image:")
+        root = tk.Tk()
+        root.withdraw()
+        root.call('wm', 'attributes', '.', '-topmost', True)
+        filename2 = askopenfilename()
+        print(filename2)
+
+        img2, size2, dimension2 = read_image(filename2)
+        img2 = Image.fromarray(img2.astype('uint8'))
+        print(f"Image selected: {os.path.basename(os.path.normpath(filename2))}")
+
+        print(f"Calculated error: {
+        np.average(abs(
+            np.array(img1, dtype="int16") -
+            np.array(img2, dtype="int16"))
+        )}")
 
 
 if __name__ == "__main__":
